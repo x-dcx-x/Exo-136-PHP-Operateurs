d5ec35beb30df3ba41d411f2e5b13d6c72de9f9d
x-dcx-x <petrisot.guillaume@gmail.com>
2023-02-14 17:07:20 +0100

    commit initial

--- index.php
+++ index.php
@@ -1,26 +1,39 @@
 <?php
 
 // Premiere ligne
-echo 10 5;
+
+$n1 = 10;
+$n2 = 5;
+$total = $n1 * $n2;
+echo $total . "<br>";
 
 //Deuxieme ligne
-echo 10 2;
+$n3 = 10;
+$n4 = 2;
+$tital = $n3 / $n4;
+echo $tital;
 
 //Troisieme ligne
-if($a $b) {
+$a = 5;
+$b = 5;
+if ($a === $b) {
     echo "<br><br>a est identique à b";
 }
 
 //Quatrieme ligne
-if($a $b) {
-    echo"<br><br>a n'est pas identique à b";
+$a = 2;
+$b = 1;
+
+if ($a > $b) {
+    echo "<br><br>a n'est pas identique à b";
 }
 
 
 //Cinquieme ligne
+
 $arr1 = ["machin", "truc"];
 $arr2 = ["truc", "machin"];
-if($arr1 $arr2) {
+if ($arr1 !== $arr2) {
     echo "<br><br>les tableaux ont le même contenu";
 }
 
